d5d0df27ba63b1c4a6207210900e48ad488114d1
MIAOHY <18752337583@163.com>
2020-05-25 22:06:31 +0800

    收藏、评价、号源

---
+++ pb_service/src/main/java/net/miaohy/pb/modules/controller/PbOrderController.java
@@ -0,0 +1,21 @@
+package net.miaohy.pb.modules.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author miaohy
+ * @since 2020-05-25
+ */
+@RestController
+@RequestMapping("/modules/pb-order")
+public class PbOrderController {
+
+}
+
